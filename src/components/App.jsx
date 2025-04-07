9554ab7cfaacf686e3fb57c28a6b983b0b7bfe14
Bogdan Kaleniuk <bogdankaleniuk95@gmail.com>
2025-04-07 17:15:14 +0300

    /////

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,28 @@ import Task1 from "./1Task/Task1";
 import Task2 from "./Task2/Task2";
 import Task3 from "./Task3/Task3";
 import Task4 from "./Task4/Task4";
+import { Routes, Route, NavLink, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      {/* <Task1 /> */}
-      {/* <Task2 /> */}
-      {/* <Task3 /> */}
-      <Task4 />
+      <div>
+        <nav>
+          <NavLink to="/task1">Task1 </NavLink>
+          <NavLink to="/task2">Task2 </NavLink>
+          <NavLink to="/task3">Task3 </NavLink>
+
+          <NavLink to="/task4">Task4 </NavLink>
+          <NavLink to="/">"Back</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/task1" element={<Task1 />} />
+          <Route path="/task2" element={<Task2 />} />
+          <Route path="/task3" element={<Task3 />} />
+          <Route path="/task4" element={<Task4 />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./components/App.jsx";
 import "modern-normalize";
 import { createContext } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 const MyContext = createContext();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyContext>
-      <App />
-    </MyContext>
+    <BrowserRouter>
+      <MyContext>
+        <App />
+      </MyContext>
+    </BrowserRouter>
   </StrictMode>
 );
